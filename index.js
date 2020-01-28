520e3ca6329d79079cc195db444620f8c1d9cea2
David Cha <davidcha17@yahoo.com>
2020-01-27 20:19:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+
+
+
+function createFareMultiplier(int) {
+    return function multiplyByX(number) {
+        return number * int; 
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+const returnFirstTwoDrivers = function(driver) {
+    return driver.slice(0,2)
+}
+
+
+const returnLastTwoDrivers = function(driver) {
+    return driver.slice(-2)
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function selectDifferentDrivers(drivers, placement){
+    return placement(drivers)
+}
+
+
+// differences between function and const
